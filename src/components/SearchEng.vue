2b72d7fa80990b1d80d52a2526ca27e591a6fb35
sahatwatz <yoke.21@hotmail.com>
2022-05-10 17:51:02 +0700

    last commit

---
+++ src/components/SearchEng.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class=" min-w-screen h-2 grid grid-cols-2 items-center">
+        <p class="text-white font-bold text-2xl pl-52 drop-shadow-lg">
+            ระบบรับสมัครนักศึกษาระดับปริญญาตรี
+        </p>
+    </div>
+    <div class="pt-0 min-w-screen h-2 items-center">
+        <div class="max-w-4xl pl-48">
+            <p class="items-start bg-gray-300 pt-2 pb-2 text-purple font-bold text-3xl pl-4  drop-shadow-lg">สถาบันเทคโนโลยีพระจอมเกล้าเจ้าคุณทหารลาดกระบัง</p>
+        </div>
+        
+    </div>
+    <div class="pt-0 min-w-screen h-2 items-center">
+        <div class="max-w- pl-96 bg-black">
+            123
+        </div>
+        
+    </div>
+</template>
